1877b2625af148bab4c1d7996a1d8145b1968ee7
Dallas James <dallasjames42@gmail.com>
2019-09-06 17:48:09 -0600

    done with showing all movies

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
-
 import SavedList from './Movies/SavedList';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+  // const addToSavedList = movie => {
+  //   setSavedList( [...savedList, movie] );
+  // };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Link exact="true" to='/'>All</Link>
+      <Link exact="true" to='movies'></Link>
+      <Route path='/' component={MovieList}></Route>
+      <Route path='movies/:id' component={Movie.id}></Route>
     </div>
   );
 };
